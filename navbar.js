ee0b168c112a69a6ee10ea56dd676f40bf3ef1f0
Nishan Paudel <neezpau04@gmail.com>
2025-12-27 08:51:58 +0545

    feat: navbar and footer through js

---
+++ navbar.js
@@ -0,0 +1,29 @@
+// Navbar Component - Reusable across all pages
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the current page filename to set active class
+  const currentPage = window.location.pathname.split('/').pop() || 'home.html';
+  
+  const navbarHTML = `
+    <div class="navbar">
+      <div class="nav-links">
+        <a href="home.html" ${currentPage === 'home.html' || currentPage === '' ? 'class="active"' : ''}><i class="fa fa-fw fa-home"></i> Home</a>
+        <a href="aboutus.html" ${currentPage === 'aboutus.html' ? 'class="active"' : ''}><i class="fa fa-fw fa-book"></i> About Us</a>
+        <a href="genre.html" ${currentPage === 'genre.html' ? 'class="active"' : ''}><i class="fa fa-fw fa-fire"></i> Genre</a>
+        <a href="bestseller.html" ${currentPage === 'bestseller.html' ? 'class="active"' : ''}><i class="fa fa-fw fa-star"></i> BestSeller</a>
+        <a href="contact.html" ${currentPage === 'contact.html' ? 'class="active"' : ''}><i class="fa fa-fw fa-envelope"></i> Contact Us</a>
+      </div>
+      <div class="search-box">
+        <i class="fa fa-search"></i>
+        <input type="text" placeholder="Search">
+      </div>
+    </div>
+  `;
+  
+  // Insert navbar at the beginning of body
+  const navbarContainer = document.getElementById('navbar-container');
+  if (navbarContainer) {
+    navbarContainer.innerHTML = navbarHTML;
+  } else {
+    document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+  }
+});
